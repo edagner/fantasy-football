0153003b8c297408e000dfa3a14432467494dcd6
ed <edagner@users.noreply.github.com>
2017-09-09 06:16:16 -0400

    created rb and receiver stat retriever and added dataframe function

--- playerStats.py
+++ playerStats.py
@@ -14,6 +14,7 @@ class PlayerAnalysis():
         self.year = year
         self.playerName = playerName
         self.gameList = []
+        self.player_data = None
 
     def listJSONFiles(self):
         gamesDir = os.path.join("GameStats{}".format(self.year))
@@ -22,42 +23,55 @@ class PlayerAnalysis():
             self.gameList.append(gameFiles)
         print self.gameList
 
-    def getReceivingStats(self):
-        receiver_data = OrderedDict()
+    def get_rb_rec_stats(self, playerType):
+        player_data = OrderedDict()
         
-        receiver_data["Names"] = []
-        receiver_data["Touchdowns"] = []
-        receiver_data["Receptions"] = []
-        receiver_data["Yards"] = []
-        
+        player_data["Name"] = []
+        player_data["Touchdowns"] = []
+        player_data["Yards"] = []
+        player_data["TwoPointConv"] = []
+        if playerType.lower() in ("receiver", "rec"):
+            player_data["Receptions"] = []
+            playerKey = u'receiving'
+        elif playerType.lower() in ("runningback", "rb"):
+            playerKey = u'rushing'
+
         for game in self.gameList:
             gameID = game.strip(".json")
             gameDir = os.path.join("GameStats{0}/{1}".format(self.year, game))
             with open(gameDir) as nfl_file:
                 data = json.load(nfl_file)
-                home_receiver = data[gameID][u'home'][u'stats'][u'receiving']
-                away_receiver = data[gameID][u'away'][u'stats'][u'receiving']
-                for rec_id in home_receiver:
-                    print rec_id, home_receiver[rec_id][u'name']
-                    receiver_data["Names"].append(home_receiver[rec_id][u'name'])
-                    receiver_data["Touchdowns"].append(home_receiver[rec_id][u'tds'])
-                    receiver_data["Receptions"].append(home_receiver[rec_id][u'rec'])
-                    receiver_data["Yards"].append(home_receiver[rec_id][u'yds'])
+                home_player = data[gameID][u'home'][u'stats'][playerKey]
+                away_player = data[gameID][u'away'][u'stats'][playerKey]
+                for rec_id in home_player:
+                    player_data["Name"].append(home_player[rec_id][u'name'])
+                    player_data["Touchdowns"].append(home_player[rec_id][u'tds'])
+                    player_data["Yards"].append(home_player[rec_id][u'yds'])
+                    player_data["TwoPointConv"].append(home_player[rec_id][u'twoptm'])
+                    if playerKey == u'receiving':
+                        player_data["Receptions"].append(home_player[rec_id][u'rec'])
 
-                for rec_id in away_receiver:
-                    print rec_id, away_receiver[rec_id][u'name']
-                    receiver_data["Names"].append(away_receiver[rec_id][u'name'])
-                    receiver_data["Touchdowns"].append(away_receiver[rec_id][u'tds'])
-                    receiver_data["Receptions"].append(away_receiver[rec_id][u'rec'])
-                    receiver_data["Yards"].append(away_receiver[rec_id][u'yds'])
-        print receiver_data
-        df = pd.DataFrame(receiver_data)
-        print df.sort_values("Names")
-        print df.groupby(['Names'])[["Touchdowns","Receptions","Yards"]].sum().plot(kind="bar")
-        pylab.show()
-
-    def getTouchdowns():
-        pass
+                for rec_id in away_player:
+                    player_data["Name"].append(away_player[rec_id][u'name'])
+                    player_data["Touchdowns"].append(away_player[rec_id][u'tds'])
+                    player_data["Yards"].append(away_player[rec_id][u'yds'])
+                    player_data["TwoPointConv"].append(away_player[rec_id][u'twoptm'])
+                    if playerKey == u'receiving':
+                        player_data["Receptions"].append(away_player[rec_id][u'rec'])
+        
+        self.player_data = player_data
+        #print self.player_data
+    
+    def statsDataFrame(self):
+        df = pd.DataFrame(self.player_data)
+        #print df.sort_values("Names")
+        #print df.groupby(['Names'])[["Touchdowns","Receptions","Yards","TwoPointConv"]].sum().head(5).plot(kind="bar")
+        #print df.groupby('Names')["Touchdowns","Yards","TwoPointConv"].sum()
+        #print df.groupby('Names').sum().sort_values(['Touchdowns','Yards'], ascending=False)
+        #print df.groupby('Name').sum().sort_values(['Touchdowns','Yards'],ascending=False).head(10)
+        print df[df['Name'] == 'D.Amendola']
+        #df["Name"]['D.Amendola']
+        #pylab.show()
 
     def playerFantasyPoints(self):
         pass
@@ -74,4 +88,5 @@ class PlayerAnalysis():
 if __name__ == '__main__':
     pa = PlayerAnalysis(2016,"T. Brady")
     pa.listJSONFiles()
-    pa.getReceivingStats()
+    pa.get_rb_rec_stats('rec')
+    pa.statsDataFrame()
